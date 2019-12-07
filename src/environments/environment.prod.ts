37f5127e8903627dbe778e5c09ad643cff647389
Daniel Nascimento Pimentel <dnprocks@yahoo.com.br>
2019-12-07 17:55:14 -0300

    Enviroments production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
+import { OperationMode } from 'json2typescript';
+
 export const environment = {
-  production: true
+  production: true,
+  API_BASE_URL: 'http://3b17f010.ngrok.io/api/v1/',
+  JSON_CONVERT_OPERATION_MODE: OperationMode.ENABLE,
 };
