9c159727d9eeb8bf36f76451eab77f1af0fb64bb
Daniel Nascimento Pimentel <dnprocks@yahoo.com.br>
2019-12-10 22:37:10 -0300

    remoção de modulo sem uso

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -2,8 +2,6 @@ import { Routes } from '@angular/router';
 import { AuthGuardService } from '../../guards/access.guard';
 
 import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
-import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
-import { TablesComponent } from '../../pages/tables/tables.component';
 
 export const AdminLayoutRoutes: Routes = [
     {   
