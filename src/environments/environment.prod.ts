aed97ee016bee3a65eeee8ec5419f241f3ef9587
Daniel Nascimento Pimentel <dnprocks@yahoo.com.br>
2019-12-11 18:57:07 -0300

    Test ngrok fix api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { OperationMode } from 'json2typescript';
 
 export const environment = {
   production: true,
-  // API_BASE_URL: 'https://37329252.ngrok.io/api/v1/',
+  API_BASE_URL: 'https://37329252.ngrok.io/api/v1/',
   API_BASE_URL_TEST: 'https://37329252.ngrok.io/api/v1/',
   JSON_CONVERT_OPERATION_MODE: OperationMode.ENABLE,
   NAME_APP: 'Central de erros'
