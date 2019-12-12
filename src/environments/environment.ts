20ffc53d0b21d53c78dadfd5efcdbe9d689031ca
Jarison Araujo <jarisonaraujo@gmail.com>
2019-12-11 23:11:32 -0300

    Customização da tela de dashboard

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@ import { OperationMode } from 'json2typescript';
 
 export const environment = {
   production: true,
-  API_BASE_URL: 'http://localhost:8080/api/v1/',
+  API_BASE_URL: 'https://66d559cc.ngrok.io/api/v1/',
   JSON_CONVERT_OPERATION_MODE: OperationMode.ENABLE,
   NAME_APP: 'Central de erros'
 };
