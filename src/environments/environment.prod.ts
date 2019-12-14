df3d7653c6a5c6eb719ca83271a690e8e74f4967
Daniel Nascimento Pimentel <dnprocks@yahoo.com.br>
2019-12-14 13:44:44 -0300

    Variaveis de ambiente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { OperationMode } from 'json2typescript';
 
 export const environment = {
   production: true,
-  API_BASE_URL: 'http://codenation-env.6m6trpeppf.us-east-2.elasticbeanstalk.com:8080/api/v1/',
+  API_BASE_URL: 'https://c558aeaf.ngrok.io/api/v1/',
   JSON_CONVERT_OPERATION_MODE: OperationMode.ENABLE,
   NAME_APP: 'Central de erros',
   EMAIL_MOCK: "dnprocks@yahoo.com.br",
