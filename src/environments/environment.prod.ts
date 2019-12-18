8961b521bc03cf0a6e4cf848f04a4cbbeb38bed5
Daniel Nascimento Pimentel <dnprocks@yahoo.com.br>
2019-12-18 00:20:02 -0300

    New api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,8 @@ import { OperationMode } from 'json2typescript';
 
 export const environment = {
   production: true,
-  API_BASE_URL: 'https://central-de-erros-api.herokuapp.com/api/v1/',
+  // API_BASE_URL: 'https://central-de-erros-api.herokuapp.com/api/v1/',
+  API_BASE_URL: 'https://codenation-api.herokuapp.com/api/v1/',
   JSON_CONVERT_OPERATION_MODE: OperationMode.ENABLE,
   NAME_APP: 'Central de erros',
   EMAIL_MOCK: "dnprocks@yahoo.com.br",
